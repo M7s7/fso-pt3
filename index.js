faed2570d3aaa264559063d3c0e377a7736bd321
Michael Siu <michaelsiu01@gmail.com>
2022-10-23 01:20:02 +1100

    Add

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const { response } = require('express')
 const express = require('express')
+const nodemon = require('nodemon')
+const morgan = require('morgan')
+
+morgan.token('newname', req => req.body.name)
+morgan.token('number', req => req.body.number)
+
 const app = express()
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms {"name":":newname", "number":":number"}'))
 
 let notes = [
   { 
@@ -32,6 +42,45 @@ app.get('/info', (req, res) => {
 app.get('/api/persons', (req, res) => {
   res.json(notes)
 })
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons/', (req, res) => {
+  const note = req.body
+
+  // Generate ID
+  const max = 10000
+  const min = 1
+  const id = Math.floor(Math.random() * (max - min) + min)
+
+  // Error handling
+  if (!note.hasOwnProperty('name')) {
+    res.status(404).end(`Error: name is missing in request!`)
+  } else if (notes.find(entries => entries.name === note.name)) {
+    res.status(404).end(`Error: the name ${note.name} already exists!`)
+  } else {
+    note.id = id
+    notes = notes.concat(note) 
+    res.json(notes)
+  }
+})
+
 
 const PORT = 3001
 app.listen(PORT, console.log(`Successfully connected to port ${PORT}`))
